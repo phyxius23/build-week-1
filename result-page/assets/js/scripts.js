7e5d67cba8573598ee1a20575666f11e7a1d6eef
DennisLoiak11 <dennisloiak11@gmail.com>
2023-02-22 10:42:42 +0100

    funzione-result-page

---
+++ result-page/assets/js/scripts.js
@@ -0,0 +1,14 @@
+
+function cerchiofrase() {
+    let percentuale = 70;
+    let frase1 = document.querySelector('.frase1');
+    let frase2 = document.querySelector('.frase2');
+    if (percentuale >= 60) {
+        document.write(frase1);
+    }else if (percentuale < 60) {
+        document.write(frase2);
+    } console.log(frase);
+}
+cerchiofrase()
+
+
