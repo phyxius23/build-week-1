2b06f03c6d2fdad3a71db8098c6487f002908334
phyxius23 <30ntin@gmail.com>
2023-02-23 15:48:23 +0100

    code cleaning

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,7 @@
+/**
+ * MATERIALE FORNITO DALLA SCUOLA
+ * ------------------------------------------------------------------------
+ */
 const questions = [
    {
       category: "Science: Computers",
@@ -94,28 +98,34 @@ const questions = [
       question:
          "Which programming language shares its name with an island in Indonesia?",
       correct_answer: "Java",
-      incorrect_answers: ["Python", "C", "Jakarta"],
-      answers: ["Java", "Python", "C", "Jakarta"],
+      incorrect_answers: ["Python", "C", "Jakarta"]
    },
 ];
 
-/** ------- test page ------- */
-/* ---------------------- ------ */
+// variabili dove salvo le risposte corrette e la quantita di domande
+let answerCorrect = [];
+let userAnswer = [];
+let userAnswersCorrect = "";
+let numberQuestions = 7;
 
-/** ------- central content ------- */
-let injectionDiv = document.querySelector('.container-js');
-
-let requests = randomQuestions(7);
-
-let results = {
-   correct: 0,
-   incorrect: 0,
-   total: requests.length
-}
+// array con all'interno delle domande random
+let requests = randomQuestions(numberQuestions);
 
 
 /**
- * funzione che sceglie random le domande dall'array questions fornito
+ * se input:checked aggiungo classe btn-start-accept che mi rende btn-start con opacity 1
+ * ------------------------------------------------------------------------
+ */
+let checkbox = document.querySelector('#checkbox');
+
+checkbox.addEventListener('click', function(){
+   document.querySelector('.btn-start').classList.toggle('btn-start-accept');
+})
+
+
+/**
+ * Fn che sceglie random le domande dall'array questions fornito
+ * ------------------------------------------------------------------------
  */
 function randomQuestions(elements){
 
@@ -136,17 +146,19 @@ function randomQuestions(elements){
    return chosenQuestions;
 }
 
+
 /**
- * funzione che unisce le risposte sbagliate e corrette e le ritorna randomizzata
+ * Fn che unisce le risposte sbagliate e corrette e le ritorna random
+ * ------------------------------------------------------------------------
  */
-function randomAnswers(numElements){
+function randomAnswers(singleObj){
 
    let chosenAnswers = [];
    let usedAnswers = [];
-   let allAnswers = numElements.incorrect_answers;
+   let allAnswers = singleObj.incorrect_answers;
 
    //array contenente tutte le possibili risposte
-   allAnswers.push(numElements.correct_answer);
+   allAnswers.push(singleObj.correct_answer);
    
    // ciclo fino a quando non ho creato una variabile contenente le risposte disposte random
    while(chosenAnswers.length < allAnswers.length){
@@ -163,133 +175,260 @@ function randomAnswers(numElements){
    return chosenAnswers;
 }
 
-/**
- * funzione con la quale creo il button e mostro una slide diversa ad ogni click
- */
-function showSlide(arrayWrapper){
-
-   let btnNextSlide = document.createElement('button');
-   let index = 0;
-
-   btnNextSlide.classList.add('btn', 'btn-next');
-   btnNextSlide.innerText = `NEXT QUESTION`;
-
-   arrayWrapper[index].classList.add('d-block');
-
-   btnNextSlide.addEventListener('click', function(){
-
-      index++;
-
-      if(index < arrayWrapper.length){
-         
-         console.log(index);
-                  
-         for(element of arrayWrapper){
-            
-            element.classList.remove('d-block');
-         }
-         arrayWrapper[index].classList.add('d-block');
-      }
-      if(index == arrayWrapper.length-1){
-
-         btnNextSlide.innerText = `Mostra il risultato`;
-      }
-   })
-
-   injectionDiv.appendChild(btnNextSlide);
-}
-
 
 /**
- * funzione che mi permetta di generare il codice html necessario per strutturare 
- * ogni singola domanda (ogni struttura la salvo in una variabile e 
- * ad ogni click vado ad eliminare quella appena visualizzata)
+ * inizio il quiz al click su .btn-start
+ * ------------------------------------------------------------------------
  */
+let start = document.querySelector('.btn-start');
+let indice = 0;
 
-function startQuiz(arrayRandom){
+start.addEventListener('click',()=>{
 
-   let counter = 0;
-   let arrayWrapper = [];
+   // se input:checked allora posso eseguire il codice che mi porta al quiz
+   if(document.querySelector('#checkbox').checked){
+
+      newQuestion(requests[indice], indice);
    
-   for(let element of arrayRandom){
-
-      let wrapperDiv = document.createElement('div'); //div contenente tutta la slide
-      let title = document.createElement("p");
-      let divQuestions = document.createElement('div'); //div contenente tutte le risposte
-      let p = document.createElement('p');
-      
-      counter++;
-
-      title.classList.add('title','title-test');
-      title.innerHTML = element.question;
-
-      //imposto le proprietà di input
-      wrapperDiv.classList.add(`slide${counter}`);
-      wrapperDiv.append(title);
-
-      // questions (flexbox)
-      divQuestions.classList.add('questions');
-
-      /**
-       * funzione che visualizza random le risposte corrette e non
-       */
-      let arrayAnswers = randomAnswers(element);
-
-      // ciclo arrayAnswers
-      for(let answer of arrayAnswers){
-
-         let divQuestion = document.createElement('div');
-         let input = document.createElement('input');
-         let label = document.createElement('label');
-         let contentLabel = document.createTextNode(answer);
-         
-         // aggiungo una classe a divQuestion
-         divQuestion.classList.add('question');
-         
-         //imposto le proprietà di input
-         input.setAttribute('type','radio');
-         input.setAttribute('name','answer');
-         input.setAttribute('id', answer.toLowerCase());
-         input.setAttribute('value', answer.toLowerCase());
-         
-         //imposto la proprietà for di label per collegarla ad input
-         label.setAttribute('for', answer.toLowerCase());
-
-         label.appendChild(contentLabel);
-         divQuestion.appendChild(input);
-         divQuestion.appendChild(label);
-         divQuestions.appendChild(divQuestion);
-      }
-  
-      p.classList.add('footer-test');
-      p.innerHTML = `Question ${counter}<span>/${arrayRandom.length}</span>`;
-
-      wrapperDiv.appendChild(divQuestions);
-
-      wrapperDiv.appendChild(p);
-      
-      arrayWrapper.push(wrapperDiv);
-
-      injectionDiv.appendChild(wrapperDiv);
+      indice++;
+   
+      document.querySelector('.btn-start').classList.add('dnone');
+      document.querySelector('.btn-next').classList.remove('dnone');   
+   }else{
+      console.log('Devi accettare le condizioni!'); //sostituire console.log() coon un messaggio nel browser
    }
 
-   showSlide(arrayWrapper);
+})
+
+
+/**
+ * passo alla domanda successiva al click su button next
+ * ------------------------------------------------------------------------
+ */
+let next = document.querySelector('.btn-next');
+// let btnResult; //punto a .btn-result => controllare se si può eliminare
+
+
+next.addEventListener('click',function(){
+
+   if(indice < requests.length){
+      newQuestion(requests[indice]);
+      indice++;
+   }
+   if(indice == requests.length){
+      next.innerText = 'Passa ai risultati';
+
+      next.addEventListener('click',function(){
+         next.textContent = 'Rate us';
+         next.classList.remove('btn-next');
+         next.classList.add('btn-rate');
+
+         checkAnswers();
+         viewResults();
+      })
+
+      // attivare appena colleghiamo pagina result
+      // next.classList.remove('btn-next');
+      // next.classList.add('btn-result');
+      // btnResult = document.querySelector('.btn-result');
+      // console.log(btnResult);
+      //return //valutare se il ramo false è necessario
+   }
+
+   if(next.classList.contains("btn-rate")){
+      // console.log('next contiene una classe btn-rate');
+      viewFeedback();
+   }
+
+})
+
+
+/**
+ * Fn che crea l'html di una nuova slide question
+ * ------------------------------------------------------------------------
+ */
+function newQuestion(answerObj){
+   
+   let target = document.querySelector('#target');
+   let html = document.querySelector('#template .slide').cloneNode(true);
+
+   let randomAnswersObj = randomAnswers(answerObj);
+
+   let idCounter = 0; //creo un contatore che utilizzerò per rendere univoco l'id di input e il for di label
+   
+   target.innerHTML = ''; //elimino il codice html contenuto nell'elemento #target
+   
+   //seleziono gli elementi
+   let titleDOM = html.querySelector('.title');
+   let answersDOM = html.querySelector('.answers');
+   let questionCounter = html.querySelector('.question-counter');
+   
+   titleDOM.textContent = answerObj.question; //inserisco la domanda all'interno del titolo
+   
+   //ciclo l'array contenente le risposte e mi creo la struttura html
+   for(let risp of randomAnswersObj){
+      idCounter++;
+      let answerClone = html.querySelector('.answer').cloneNode();
+      let inputClone = html.querySelector('.answer .input').cloneNode();
+      let optionClone = html.querySelector('.answer .option').cloneNode();
+      
+      inputClone.setAttribute('id', `input${idCounter}`);
+      inputClone.setAttribute('value', risp);
+      
+      optionClone.textContent = risp;
+      optionClone.setAttribute('for', `input${idCounter}`);
+      
+      answerClone.append(inputClone);
+      answerClone.append(optionClone);
+      answersDOM.append(answerClone);
+   }
+
+   // rimuovo gli elementi vuoti che ho usato per clonarli
+   html.querySelector('.answer').remove(); //rimuovo la prima answer
+   html.querySelector('.slide .question-counter').remove(); //rimuovo la prima question-counter
+   
+   questionCounter.innerHTML = `Question ${indice+1}<span>/${requests.length}</span>`;
+   
+   html.append(questionCounter); //aggiungo come ultimo figlio l'elemento questionCounter
+   target.append(html); //inserisco il codice html generato all'interno dell'elemento #target
+
+   saveAnswer(html);
+   answerCorrect.push(answerObj.correct_answer);
 }
-startQuiz(randomQuestions(7));
+
 
 /**
- * provare ad implementare le nuove funzionalità mostrate da Michele questa sera
+ * Fn che mi conta le risposte esatte date dall'utente
+ * ------------------------------------------------------------------------
  */
+function checkAnswers(){
+   
+   if(userAnswer != ''){
+      
+      for(let i = 0; i < userAnswer.length; i++){
+
+         if(answerCorrect[i].includes(userAnswer[i])){ //se answerCorrect = userAnswer
+         
+            userAnswersCorrect += ".";           //aggiungo un punto alla stringa
+         }   
+      }
+   }
+}
+
 
 /**
- * TIMER
+ * Fn che salva la risposta dell'utente in una variabile globale
+ * ------------------------------------------------------------------------
  */
-// let count = 20;
+function saveAnswer(html) {
 
-// let timer = setInterval(function(){
-//                console.log(count);//mostro il valore aggiornato
-//                if(count == 0){
-//                      clearInterval(timer)//fermo il timer
-//                }
-//                count--;//riduco count di 1
-//             },1000)
+   let inputs = html.querySelectorAll('.slide input'); //seleziono tutti gli input interni alla slide
+
+   // salva la risposta dell'utente
+   for(let input of inputs){
+
+      input.addEventListener('click', function(){
+      
+         userAnswer.push(input.value); //salvo la risposta dell'utente in userAnswer
+      })
+   }
+}
+
+
+/**
+ * Fn che mi genera il codice html della pagina results
+ * ------------------------------------------------------------------------
+ */
+function viewResults(){
+   
+   let target = document.querySelector('#target');
+   let html = document.querySelector('#template-results .main-results').cloneNode(true);
+
+   let percentuale = Math.floor((100/numberQuestions) * userAnswersCorrect.length);
+
+   target.innerHTML = "";
+
+   // seleziono elementi 
+   let resultsText = html.querySelector('.results__center .results__text');
+
+   // seleziono gli elementi results__percent & results__questions di results__left
+   let resultsPercent = html.querySelector('.results__left .results__percent');
+   let resultsQuestions = html.querySelector('.results__left .results__questions');
+   // html.querySelector('.results__left .results__percent').textContent = `${percentuale}%`;
+
+   // inserisco contenuto su results__percent & results__questions di results__left
+   resultsPercent.textContent = `${percentuale}%`;
+   resultsQuestions.textContent = `${userAnswersCorrect.length}/${numberQuestions} questions`;
+
+   // seleziono gli elementi results__percent & results__questions di results__right
+   resultsPercent = html.querySelector('.results__right .results__percent');
+   resultsQuestions = html.querySelector('.results__right .results__questions');
+
+   // inserisco contenuto su results__percent & results__questions di results__right
+   resultsPercent.textContent = `${100 - percentuale}%`;
+   resultsQuestions.textContent = `${numberQuestions - userAnswersCorrect.length}/${numberQuestions} questions`;
+
+   if (percentuale >= 60) {
+      resultsText.innerHTML = `<b>Congratulations!</b><br><b class="bold-color">You passed the exam</b><p>We'll send you the certificate<br>in few minutes<br>check your email (including<br>promotions / spam folder)</p>`;
+   }else if (percentuale < 60) {
+      resultsText.innerHTML = "Non hai superato l'esame";
+   }
+
+   target.append(html);
+}
+
+
+/**
+ * Fn che mi genera il codice html della pagina feedback
+ * ------------------------------------------------------------------------
+ */
+function viewFeedback(){
+   
+   let target = document.querySelector('#target');
+   let html = document.querySelector('#template-feedback .main-feedback').cloneNode(true);
+
+   target.innerHTML = "";
+
+   // seleziono il contenitore delle stelle
+   let starsContainer = html.querySelector('.stars');
+   
+   // itero per creare le 10 stelle di cui ho bisogno
+   for(let i = 0; i < 10; i++){
+      
+      let star = html.querySelector('.star').cloneNode(true);
+      starsContainer.append(star);
+   }
+
+   // rimuovo gli elementi vuoti usati per clonarli
+   html.querySelector('.star').remove(); //rimuovo la prima stella
+      
+   target.append(html);
+
+   // avvio la Fn che mi permette di colorare le stelle
+   colorStars(html);
+}
+
+
+/**
+ * Fn per colorare le stelle
+ * ------------------------------------------------------------------------
+ */
+function colorStars(html){
+
+   let stars = html.querySelectorAll('.star');
+
+   for(let i = 0; i < stars.length; i++){
+
+      stars[i].addEventListener('click', function(){
+
+         for(let star of stars){
+
+            star.style.fill = '';
+         }
+         for(let y = 0; y <= i; y++){
+
+            stars[y].style.fill = '#00FFFF'
+         }
+      })
+   }
+}
